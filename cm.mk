838812f953fd05c8e61915ad35d7b871cb6d86bf
dadcup18 <dadcup18@users.noreply.github.com>
2014-06-08 01:25:06 -0400

    revert this

--- cm.mk
+++ cm.mk
@@ -30,4 +30,4 @@ PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint and ID
 BUILD_ID := TRIANA00$(shell date -u +%Y%m%d)
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=cm_codinalte BUILD_FINGERPRINT=cyanogenmod/cm_codinalte/codinalte:4.4.3/$(BUILD_ID) PRIVATE_BUILD_DESC="SGH-T599X 4.4.3"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=cm_codinalte BUILD_FINGERPRINT=cyanogenmod/cm_codinalte/codinalte:4.4.2/$(BUILD_ID) PRIVATE_BUILD_DESC="SGH-T599X 4.4.2"
